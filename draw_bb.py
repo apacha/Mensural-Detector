2f8c8a51485ae053d8227a7c404e104068495683
Jorge Calvo <jcalvo@dlsi.ua.es>
2018-01-24 09:29:30 +0100

    Tool for visual ground-truth check

---
+++ draw_bb.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2
+import argparse
+
+# -------------------------------------------------------------
+
+parser = argparse.ArgumentParser(description='Draw the bounding boxes from the ground-truth data.')
+parser.add_argument('-img',     dest='img_path',     type=str,  required=True, help='Path to the image.')
+parser.add_argument('-gt',      dest='gt_path',  type=str,  required=True, help='Path to the ground truth.')
+parser.add_argument('-save',    dest='save_img',     type=str,  required=True, help='Path to save the processed image.')
+args = parser.parse_args()
+
+
+
+img = cv2.imread(args.img_path,True)
+
+
+gt_file = file(args.gt_path,'r')
+lines = gt_file.read().splitlines()
+gt_file.close()
+
+for line in lines:
+    upper_left, lower_right, gt_shape, gt_position = line.split(';')
+    
+    x1,y1 = upper_left.split(',')
+    x2,y2 = lower_right.split(',')
+    
+    # String to float, float to int
+    x1 = int(float(x1))    
+    y1 = int(float(y1))
+    x2 = int(float(x2))
+    y2 = int(float(y2))
+    
+    cv2.rectangle(img,(x1,y1),(x2,y2),(0,0,0),3)    
+    cv2.putText(img,gt_shape + '/' + gt_position,(x1,y1), cv2.FONT_HERSHEY_PLAIN, 1,(255,0,0),1)
+
+cv2.imwrite(args.save_img,img)
+
+
