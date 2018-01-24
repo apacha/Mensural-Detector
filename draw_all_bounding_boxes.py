4c7b62018ebf63aa0fa347f08ec84854bf86d618
Alexander Pacha <alexander.pacha@gmail.com>
2018-01-24 17:02:39 +0100

    Rewrote draw_bounding_boxes as function and added script to automatically annotate all images from a given directory.

---
+++ draw_all_bounding_boxes.py
@@ -0,0 +1,29 @@
+import argparse
+
+import os
+
+from tqdm import tqdm
+
+from draw_bounding_boxes import draw_bounding_boxes_into_image
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Draws the bounding boxes for all image')
+    parser.add_argument('-dataset_directory', dest='dataset_directory', type=str, default="db",
+                        help='Path to the folder containing all images and annotations')
+    args = parser.parse_args()
+
+    dataset_directory = args.dataset_directory
+    files = os.listdir(dataset_directory)
+    images = [file for file in files if file.endswith(".JPG")]
+    annotations = [file for file in files if file.endswith(".txt")]
+    pairs = zip(images, annotations)
+
+    output_directory = "output"
+    os.makedirs(output_directory, exist_ok=True)
+
+    for pair in tqdm(pairs, desc="Generating annotated images", total=len(images)):
+        image = os.path.join(dataset_directory, pair[0])
+        annotations = os.path.join(dataset_directory, pair[1])
+        destination_file = os.path.join(output_directory,
+                                        os.path.splitext(os.path.basename(image))[0] + "_annotated.JPG")
+        draw_bounding_boxes_into_image(image, annotations, destination_file)
--- draw_bounding_boxes.py
+++ draw_bounding_boxes.py
@@ -2,32 +2,36 @@ import numpy as np
 import cv2
 import argparse
 
-# -------------------------------------------------------------
 
-parser = argparse.ArgumentParser(description='Draw the bounding boxes from the ground-truth data.')
-parser.add_argument('-img', dest='img_path', type=str, required=True, help='Path to the image.')
-parser.add_argument('-gt', dest='gt_path', type=str, required=True, help='Path to the ground truth.')
-parser.add_argument('-save', dest='save_img', type=str, required=True, help='Path to save the processed image.')
-args = parser.parse_args()
+def draw_bounding_boxes_into_image(image_path: str, ground_truth_annotations_path: str, destination_path: str):
+    img = cv2.imread(image_path, True)
 
-img = cv2.imread(args.img_path, True)
+    with open(ground_truth_annotations_path, 'r') as gt_file:
+        lines = gt_file.read().splitlines()
 
-with open(args.gt_path, 'r') as gt_file:
-    lines = gt_file.read().splitlines()
+    for line in lines:
+        upper_left, lower_right, gt_shape, gt_position = line.split(';')
 
-for line in lines:
-    upper_left, lower_right, gt_shape, gt_position = line.split(';')
+        x1, y1 = upper_left.split(',')
+        x2, y2 = lower_right.split(',')
 
-    x1, y1 = upper_left.split(',')
-    x2, y2 = lower_right.split(',')
+        # String to float, float to int
+        x1 = int(float(x1))
+        y1 = int(float(y1))
+        x2 = int(float(x2))
+        y2 = int(float(y2))
 
-    # String to float, float to int
-    x1 = int(float(x1))
-    y1 = int(float(y1))
-    x2 = int(float(x2))
-    y2 = int(float(y2))
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 0), 3)
+        cv2.putText(img, gt_shape + '/' + gt_position, (x1, y1), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
 
-    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 0), 3)
-    cv2.putText(img, gt_shape + '/' + gt_position, (x1, y1), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
+    cv2.imwrite(destination_path, img)
 
-cv2.imwrite(args.save_img, img)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Draw the bounding boxes from the ground-truth data.')
+    parser.add_argument('-img', dest='img_path', type=str, required=True, help='Path to the image.')
+    parser.add_argument('-gt', dest='gt_path', type=str, required=True, help='Path to the ground truth.')
+    parser.add_argument('-save', dest='save_img', type=str, required=True, help='Path to save the processed image.')
+    args = parser.parse_args()
+
+    draw_bounding_boxes_into_image(args.img_path, args.gt_path, args.save_img)
