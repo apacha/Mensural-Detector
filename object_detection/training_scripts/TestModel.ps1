937c410441e02b598d2394d168db090da30a424d
Alexander Pacha <alexander.pacha@gmail.com>
2018-02-23 17:15:09 +0100

    Updated paths in trainings_scripts

--- object_detection/training_scripts/TestModel.ps1
+++ object_detection/training_scripts/TestModel.ps1
@@ -1,12 +1,23 @@
 $pathToGitRoot = "C:/Users/Alex/Repositories/Mensural-Detector"
-$pathToSourceRoot = "$($pathToGitRoot)/tensorflow_object_detection"
-$pathToTranscript = "$($pathToSourceRoot)/Transcripts"
+$pathToSourceRoot = "$($pathToGitRoot)/object_detection"
+$pathToTranscript = "$($pathToSourceRoot)/transcripts"
 cd $pathToSourceRoot
+
+# Make sure that python finds all modules inside this directory
+echo "Appending required paths to temporary PYTHONPATH"
+$env:PYTHONPATH = "$($pathToGitRoot);$($pathToSourceRoot);$($pathToGitRoot)/slim"
 
 ################################################################
 # Available configurations - uncomment the one to actually run #
 ################################################################
-$configuration = "faster_rcnn_inception_resnet_v2_atrous"
+# $configuration = "faster_rcnn_inception_resnet_v2_atrous"
+# $configuration = "faster_rcnn_inception_resnet_v2_atrous_600_proposals"
+# $configuration = "faster_rcnn_inception_resnet_v2_atrous_600_proposals_pretrained"
+# $configuration = "faster_rcnn_inception_resnet_v2_atrous_1200_proposals"
+# $configuration = "faster_rcnn_inception_resnet_v2_atrous_1200_proposals_max_suppr_09"
+$configuration = "faster_rcnn_inception_resnet_v2_atrous_1200_proposals_max_suppr_03"
+#$configuration = "faster_rcnn_inception_resnet_v2_atrous_1200_proposals_only_rpn"
+
 
 
 Start-Transcript -path "$($pathToTranscript)/TestModel-$($configuration).txt" -append
