a0d5f1ea717ea44c27d235d491b3eecfac5a5780
Alexander Pacha <alexander.pacha@gmail.com>
2018-03-06 13:31:56 +0100

    Updated training-script to also create the dataset

--- position_classification/train_model.ps1
+++ position_classification/train_model.ps1
@@ -5,6 +5,15 @@ cd $pathToSourceRoot
 echo "Appending source root $($pathToSourceRoot) to temporary PYTHONPATH"
 $env:PYTHONPATH = $env:PYTHONPATH;"$($pathToSourceRoot)\"
 
-Start-Transcript -path "$($pathToTranscript)\2018-05-03_vgg4_128x448_mb8.txt" -append
+# Run on first time
+python extract_sub_image_for_classification.py
+python dataset_splitter.py
+
+Start-Transcript -path "$($pathToTranscript)\2018-05-03_vgg4_128x448_mb16.txt" -append
 python "$($pathToSourceRoot)\train_model.py" --model_name vgg4 --width 128 --height 448
+Stop-Transcript
+
+exit
+Start-Transcript -path "$($pathToTranscript)\2018-05-03_res_net_4_128x448_mb16.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --model_name res_net_4 --width 128 --height 448
 Stop-Transcript
