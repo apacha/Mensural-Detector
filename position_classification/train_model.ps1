52f035230fc9d9ef2265e75353de2b348ad09d33
Alexander Pacha <alexander.pacha@gmail.com>
2018-03-20 17:04:58 +0100

    Adding previous run configurations

--- position_classification/train_model.ps1
+++ position_classification/train_model.ps1
@@ -7,18 +7,34 @@ cd $pathToSourceRoot
 echo "Appending required paths to temporary PYTHONPATH"
 $env:PYTHONPATH = "$($pathToGitRoot);$($pathToSourceRoot)"
 
-Start-Transcript -path "$($pathToTranscript)\2018-03-20_vgg_global_average_160x448_mb8.txt" -append
+Start-Transcript -path "$($pathToTranscript)\2018-03-20_inception_resnet_v2_160x448_mb8.txt" -append
 
 # Delete and re-create dataset directory
 Remove-Item -Recurse -Force data
 python extract_sub_image_for_classification.py --width 160 --height 448 --group_by staff_position
 python dataset_splitter.py
 
-python "$($pathToSourceRoot)\train_model.py" --model_name vgg_global_average --width 160 --height 448
+python "$($pathToSourceRoot)\train_model.py" --model_name inception_resnet_v2 --width 160 --height 448
 Stop-Transcript
 
 
 exit
+
+Start-Transcript -path "$($pathToTranscript)\2018-03-06_vgg4_128x448_mb16_reduced_classes.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --model_name vgg4 --width 128 --height 448
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)\2018-03-06_vgg4_128x448_mb16_reduced_classes_sklearn_balance.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --model_name vgg4 --width 128 --height 448
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)\2018-03-05_vgg4_128x448_mb16.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --model_name vgg4 --width 128 --height 448
+Stop-Transcript
+
+Start-Transcript -path "$($pathToTranscript)\2018-03-05_res_net_4_128x448_mb16.txt" -append
+python "$($pathToSourceRoot)\train_model.py" --model_name res_net_4 --width 128 --height 448
+Stop-Transcript
 
 Start-Transcript -path "$($pathToTranscript)\2018-05-03_vgg4_128x448_mb16.txt" -append
 python "$($pathToSourceRoot)\train_model.py" --model_name vgg4 --width 128 --height 448
