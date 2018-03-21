2f8a4aa677af7dbf68e323305c6db46f95001a6d
Alexander Pacha <alexander.pacha@gmail.com>
2018-03-21 18:01:48 +0100

    Cleaned up training script and simplified it

--- position_classification/train_model.ps1
+++ position_classification/train_model.ps1
@@ -7,47 +7,33 @@ cd $pathToSourceRoot
 echo "Appending required paths to temporary PYTHONPATH"
 $env:PYTHONPATH = "$($pathToGitRoot);$($pathToSourceRoot)"
 
-Start-Transcript -path "$($pathToTranscript)\2018-03-21_inception_resnet_v2_pretrained_160x448_group_by_classes.txt" -append
+$dataset_directory = "data"
+$width = 160
+$height = 448
 
-# Delete and re-create dataset directory
-$dataset_directory = "data_classes"
-Remove-Item -Recurse -Force $dataset_directory
-python extract_sub_image_for_classification.py --output_directory $dataset_directory --width 160 --height 448 --group_by class_name
-python dataset_splitter.py --source_directory $dataset_directory --destination_directory $dataset_directory
+## Pick by what criterion the samples should be grouped by 
+## (staff_position for pitch-detection, class_name for general symbol classification, e.g. for finding incorrectly labelled symbols)
+$group_by = "staff_position"
+#$group_by = "class_name"
 
-python "$($pathToSourceRoot)\train_model.py" --dataset_directory $dataset_directory --model_name inception_resnet_v2_pretrained --width 160 --height 448 --output_name 2018-03-21_inception_resnet_v2_pretrained_160x448_group_by_classes
-Stop-Transcript
+$models = @() # create empty array
+## Pick The ones, you want to run
+# $models += "inception_resnet_v2_pretrained"
+# $models += "inception_resnet_v2"
+# $models += "res_net_4"
+# $models += "res_net_50"
+$models += "dense_net_201"
+# $models += "vgg4"
+$models += "vgg16"
 
-exit
+foreach ($model in $models) {
+	# Run a training per selected model
+	$transcript_path = "$($pathToTranscript)\2018-03-21_$($model_name)_$($width)x$($height).txt"
 
-Start-Transcript -path "$($pathToTranscript)\2018-03-21_inception_resnet_v2_pretrained_160x448.txt" -append
-python "$($pathToSourceRoot)\train_model.py" --model_name inception_resnet_v2_pretrained --width 160 --height 448
-Stop-Transcript
-
-Start-Transcript -path "$($pathToTranscript)\2018-03-20_inception_resnet_v2_160x448_mb8.txt" -append
-python "$($pathToSourceRoot)\train_model.py" --model_name inception_resnet_v2 --width 160 --height 448
-Stop-Transcript
-
-Start-Transcript -path "$($pathToTranscript)\2018-03-06_vgg4_128x448_mb16_reduced_classes.txt" -append
-python "$($pathToSourceRoot)\train_model.py" --model_name vgg4 --width 128 --height 448
-Stop-Transcript
-
-Start-Transcript -path "$($pathToTranscript)\2018-03-06_vgg4_128x448_mb16_reduced_classes_sklearn_balance.txt" -append
-python "$($pathToSourceRoot)\train_model.py" --model_name vgg4 --width 128 --height 448
-Stop-Transcript
-
-Start-Transcript -path "$($pathToTranscript)\2018-03-05_vgg4_128x448_mb16.txt" -append
-python "$($pathToSourceRoot)\train_model.py" --model_name vgg4 --width 128 --height 448
-Stop-Transcript
-
-Start-Transcript -path "$($pathToTranscript)\2018-03-05_res_net_4_128x448_mb16.txt" -append
-python "$($pathToSourceRoot)\train_model.py" --model_name res_net_4 --width 128 --height 448
-Stop-Transcript
-
-Start-Transcript -path "$($pathToTranscript)\2018-05-03_vgg4_128x448_mb16.txt" -append
-python "$($pathToSourceRoot)\train_model.py" --model_name vgg4 --width 128 --height 448
-Stop-Transcript
-
-Start-Transcript -path "$($pathToTranscript)\2018-05-03_res_net_4_128x448_mb16.txt" -append
-python "$($pathToSourceRoot)\train_model.py" --model_name res_net_4 --width 128 --height 448
-Stop-Transcript
+	Start-Transcript -path $transcript_path -append
+	Remove-Item -Recurse -Force $dataset_directory
+	python extract_sub_image_for_classification.py --output_directory $dataset_directory --width $width --height $height --group_by $group_by
+	python dataset_splitter.py --source_directory $dataset_directory --destination_directory $dataset_directory
+	python "$($pathToSourceRoot)\train_model.py" --dataset_directory $dataset_directory --model_name $model_name --width $width --height $height
+	Stop-Transcript
+}
