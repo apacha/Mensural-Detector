8172d417193d7aacb338307f09a27ae56c403785
Alexander Pacha <alexander.pacha@gmail.com>
2018-03-30 12:55:44 +0200

    Adding script that automatically builds protobuf files and library.

---
+++ object_detection/DownloadAndBuildProtocolBuffers.ps1
@@ -0,0 +1,39 @@
+$pathToSourceRoot = "C:\Users\Alex\Repositories\Mensural-Detector\object_detection\"
+$pathToTranscript = "$($pathToSourceRoot)"
+
+cd $pathToSourceRoot
+echo "Appending source root $($pathToSourceRoot) to temporary PYTHONPATH"
+$env:PYTHONPATH = $pathToSourceRoot
+
+Start-Transcript -path "$($pathToTranscript)Transcript.txt" -append
+
+# Compile Protoc files
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
+$url = "https://github.com/google/protobuf/releases/download/v3.4.0/protoc-3.4.0-win32.zip"
+$output = $pathToSourceRoot + "protoc-3.4.0-win32.zip"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Invoke-WebRequest -Uri $url -OutFile $output
+
+$protoc_folder = $pathToSourceRoot + "Protoc"
+Unzip $output $protoc_folder
+
+.\Protoc\bin\protoc.exe --version
+
+cd ..
+.\object_detection\Protoc\bin\protoc.exe object_detection/protos/*.proto --python_out=.
+cd slim
+python setup.py install
+cd ..
+python setup.py install
+
+rm object_detection/Protoc -Recurse
+rm object_detection/protoc-3.4.0-win32.zip
+
+Stop-Transcript
